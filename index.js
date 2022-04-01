b4bca0212269972178006a12a19791b5498c8041
shubham sarode <inter.shubham@sangnak.in>
2022-04-01 15:07:38 +0530

    console log the data in form

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const canvasElement = document.getElementById("canvas");
 const snapSoundElement = document.getElementById("snapSound");
 const btn = document.getElementById("openCamera");
 let takeSnap = document.getElementById("take-snap");
-const cameraList = document.getElementById("camera-list");
+let cameraList = document.getElementById("camera-list");
+let picture;
 let newUser = "user";
 const webcam = new Webcam(
   webcamElement,
@@ -14,15 +15,20 @@ const webcam = new Webcam(
 
 console.log(webcam);
 
-
 //onclick on oencamera
 
 btn.addEventListener("click", initial, false);
 function initial() {
-  console.log("click");
+  // if (select.value === '') {
+  //   videoConstraints.facingMode = 'environment';
+  // } else {
+  //   videoConstraints.deviceId = { exact: select.value };
+  // }
+  // console.log("click");
   if (btn.innerHTML == "close camera") {
     stopCamera();
     btn.innerHTML = "open camera";
+    cameraList.appendChild("");
   } else if (btn.innerHTML == "open camera") {
     startCamera();
     console.log("camera is started");
@@ -31,12 +37,8 @@ function initial() {
   }
 }
 
-
-
-
 //start camera fucntion
 function startCamera() {
-  genarate();
   console.log("click");
   webcam
     .start()
@@ -52,9 +54,9 @@ function startCamera() {
   takeSnap.style.opacity = "1";
   webcamElement.style.opacity = "1";
   cameraList.style.display = "block";
+  genarate();
+  cameraList.appendChild("");
 }
-
-
 
 //to stop the camera
 
@@ -68,19 +70,17 @@ function stopCamera() {
   takeSnap.style.opacity = "0";
 }
 
-
 //to take the picture
 function takePicture() {
-  let picture = webcam.snap();
+  picture = webcam.snap();
+
   console.log(picture);
   stopCamera();
   btn.style.display = "none";
   takeSnap.style.display = "none";
 }
 
-
-
-//to get the camera list 
+//to get the camera list
 // console.log(navigator.mediaDevices.enumerateDevices());
 
 let labels = [];
@@ -98,9 +98,9 @@ navigator.mediaDevices
             " group id = " +
             device.groupId
         );
-
+        webcam.webcamList.push(device.deviceId);
         labels.push(device.label);
-        console.log(labels);
+        console.log(labels, webcam.webcamList);
       }
     });
   })
@@ -112,16 +112,32 @@ const genarate = () => {
   select.name = "camera lists";
   select.id = "camera";
   console.log(labels);
-  let count =0
+  let count = 0;
   labels.forEach((val) => {
     var option = document.createElement("option");
-    option.value =  val !='' ? val:'camera'+count;
-    option.text =  val !='' ? val:'camera'+count;
+    option.value = val != "" ? val : "camera " + count;
+    option.text = val != "" ? val : "camera " + count;
     select.appendChild(option);
   });
   var label = document.createElement("label");
   label.innerHTML = "Choose your camera: ";
   label.htmlFor = "camera";
-
   cameraList.appendChild(label).appendChild(select);
 };
+
+//collect the data
+let fname = document.getElementById("fname");
+let lname = document.getElementById("lname");
+let number = document.getElementById("number");
+
+function handleSubmit() {
+  // e.preventDefault();
+  console.log("First Name : "+ fname.value);
+  console.log("Last Name : "+ lname.value);
+  console.log("Contact  Number : "+ number.value);
+  console.log("image:"+ picture);
+  fname.value = "";
+  lname.value = "";
+  number.value = "";
+  picture = "";
+}
